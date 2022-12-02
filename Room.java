4bb5a9debedba8b1ce9ec881203274a7958ce252
JOANNA LI <jli40@smith.edu>
2022-12-01 22:04:22 -0500

    Ditto

--- Room.java
+++ Room.java
@@ -4,11 +4,66 @@ import java.util.*;
  */
 public class Room {
     //Hashtable rooms = new Hashtable<Integer, String[]>();
-    ArrayList<Item> roomInventory = new ArrayList<Item>();
+    private int roomID; 
+    private String name; 
+    private String descrip; 
+    private ArrayList<Item> roomInventory = new ArrayList<Item>();
 
-    public Room( ArrayList<Item> roomInventory) {
-        this.roomInventory = roomInventory; 
+    /*Constructor for Rooms */
+    public Room(int roomID, String name) {
+        this.roomID = roomID; 
+        this.name = name; 
     }
+
+    /**
+     * Returns the name of a Room
+     * @return String name of a Room
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the description of a Room
+     * @return String description of a Room
+     */
+    public String getDescrip() {
+        return descrip; 
+    }
+
+    /**
+     * Returns the ID of a Room
+     * @return int ID of a Room
+     */
+    public int getRoomID() {
+        return roomID; 
+    }
+
+    
+    /**
+     * Sets the name of a Room
+     * @param new String name of a Room
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets the description of a Room
+     * @param new String description of a Room
+     */
+    public void setDescrip(String descrip) {
+        this.descrip = descrip; 
+    }
+
+    /**
+     * Sets the room ID of a Room
+     * @param new int room ID of a Room
+     */
+    public void setRoomID(int roomID) {
+        this.roomID = roomID; 
+    }
+
     /**
      * Main method for testing 
      * @param args Array for command line arguments which is not currently being used  
