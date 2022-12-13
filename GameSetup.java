d6c799db34120651286155b1e17cc4d56f839d87
JOANNA LI <jli40@smith.edu>
2022-12-13 00:30:44 -0500

    Added more room exits

--- GameSetup.java
+++ GameSetup.java
@@ -1,8 +1,10 @@
 public class GameSetup { 
     Room outside = new Room("outside Elsie's House", "From the view on your screen, this digital model of Elsie's yellow stucco bungalow looks just like a real photo");
-    Room foyer = new Room("in the foyer", "A room for receiving guests, not that Elsie would have had many visitor in her last days");
-        public void startGame() { 
-            outside.addRoomExit("NORTH", foyer); //does not compile because i cannot put this enum into the Map exits
+    Room foyer = new Room("in the foyer", "A room for receiving guests, not that Elsie would have had too many in her last days");
+       
+        public void setUpExits() { 
+            outside.addRoomExit("SOUTH", foyer);
+            foyer.addRoomExit("NORTH", outside );
         }
     public static void main(String[] args) {
 
